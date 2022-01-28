9366a313c5f3b10567f4e5b4f88cb855fd84bb17
VaishaliUs <vaishaliu1713@gmail.com>
2022-01-28 23:48:44 +0530

    New commit

--- flip.sh
+++
@@ -1,7 +0,0 @@
-
-coinflip=$(($(($RANDOM%10))%2))
-if [ $coinflip -eq 1 ];then
-    echo "heads"
-else
-    echo "tails"
-fi
--- forLoop.sh
+++
@@ -1,4 +0,0 @@
-for (( cnt=1; cnt<=10; cnt=$((cnt+2))))
-do
-	echo $cnt;
-done
